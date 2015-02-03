fc1a1afab58f9345cc6c37405b9c8a429271da0b
Pete Cheslock <petecheslock@gmail.com>
2015-02-02 19:55:12 -0500

    Fix broken spec tests

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -4,7 +4,7 @@ describe 'threatstack::configure' do
   let(:facts) { {:osfamily => 'Debian'} }
 
   it { should contain_exec('configure-threatstack-agent').with(
-    :command => "/opt/threatstack/bin/cloudsight setup --deploy-key=xKkRzes --policy='Default Policy'"
+    :command => "/opt/threatstack/bin/cloudsight setup --deploy-key=xKkRzes --policy='Default Policy' --hostname='test-hostname'"
   )}
 
 end
--- spec/fixtures/manifests/site.pp
+++ spec/fixtures/manifests/site.pp
@@ -1,3 +1,4 @@
 class { 'threatstack':
   deploy_key => 'xKkRzes',
+  ts_hostname => 'test-hostname'
 }
