8098bc8cea5d8e5f4dbea06dd76bdc66c5bebe54
Pete Cheslock <petecheslock@gmail.com>
2015-08-25 17:47:12 -0400

    Use ruleset and default to Base Rule Set

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -8,7 +8,7 @@
 class threatstack::configure {
 
   exec { 'configure-threatstack-agent':
-    command     => "/opt/threatstack/bin/cloudsight setup --deploy-key=${threatstack::deploy_key} --policy='${threatstack::policy}' --hostname='${threatstack::ts_hostname}'",
+    command     => "/opt/threatstack/bin/cloudsight setup --deploy-key=${threatstack::deploy_key} --ruleset='${threatstack::policy}' --hostname='${threatstack::ts_hostname}'",
     subscribe   => Package[$threatstack::ts_package],
     refreshonly => true,
   }
