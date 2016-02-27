94dc2990145727bbe7b120d0f36032f2ce6c20cc
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-02-27 17:33:13 -0500

    need site.pp to no longer be ignored since it's a part of our test suite
    IN-519: https://threatstack.atlassian.net/browse/IN-519

---
+++ manifests/site.pp
@@ -0,0 +1,27 @@
+# == Class: threatstack::site
+#
+# For use by Puppet test-kitchen only.
+#
+# === Authors
+#
+# Pete Cheslock <pete.cheslock@threatstack.com>
+# Tom McLaughlin <tom.mclaughlin@threatstack.com>
+#
+# === Copyright
+#
+# Copyright 2016 Threat Stack, Inc.
+#
+include ::stdlib
+
+# Used to test package_version parameter works.
+if $::osfamily == 'RedHat' {
+  $package_version = "1.4.2-1.1.el${::operatingsystemmajrelease}"
+} elsif $::osfamily == 'Debian' {
+  $pkg_os = downcase($::operatingsystem)
+  $package_version = "1.4.2.0${pkg_os}14.1"
+}
+
+class { '::threatstack':
+  deploy_key      => 'MyKey',
+  package_version => $package_version
+}
