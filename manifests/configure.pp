1fa9e5c13ce6e8d522701d4456fa010b1aa6966e
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-02-27 16:59:32 -0500

    Make test-kitchen pass locally.
    IN-519: https://threatstack.atlassian.net/browse/IN-519

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -21,10 +21,18 @@ class threatstack::configure {
   $rulesets     = $threatstack::ruleset # bring value into scope.
   $ruleset_args = inline_template("<% @rulesets.each do |ruleset| -%> --ruleset='<%= ruleset %>'<% end -%>")
 
+  # We can't configure the agent without error in a testing environment.
+  if $::ts_test_kitchen != undef {
+    $return_codes = [0, 1]
+  } else {
+    $return_codes = [0]
+  }
+
   exec { 'configure-threatstack-agent':
     command     => "/opt/threatstack/bin/cloudsight setup --deploy-key='${::threatstack::deploy_key}' --hostname='${::threatstack::ts_hostname}' ${ruleset_args}",
     subscribe   => Package[$threatstack::ts_package],
     refreshonly => true,
+    returns     => $return_codes,
     path        => '/usr/bin'
   }
 
