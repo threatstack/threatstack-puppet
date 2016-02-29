187cde634c987b7c634246af4c6aa7133d0e4046
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-02-29 11:38:26 -0500

    Fixed test that shouldn't have broken.
    IN-519: https://threatstack.atlassian.net/browse/IN-519

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe 'threatstack::configure' do
 
   deploy_key = ENV['TS_DEPLOY_KEY'] ? ENV['TS_DEPLOY_KEY'] : "xKkRzesqg"
+  ts_hostname = 'test-host'
 
   context 'on Debian' do
     let(:facts) { {:osfamily => 'Debian'} }
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}' }" }
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', ruleset => ['Default Ruleset', 'Service Ruleset'] }" }
 
     it { should contain_exec('configure-threatstack-agent').with(
-      :command => "/opt/threatstack/bin/cloudsight setup --deploy-key='#{deploy_key}' --hostname='test-host'  --ruleset='Default Ruleset' --ruleset='Service Ruleset'"
+      :command => "/opt/threatstack/bin/cloudsight setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}'  --ruleset='Default Ruleset' --ruleset='Service Ruleset'"
     )}
   end
 
