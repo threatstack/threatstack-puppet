07638fe2bb7f1f9c023c1fa993629a6e256ee600
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-04-15 18:46:20 -0400

    allow passing arbitrary agent args.
    Useful for enabling new features.

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -22,7 +22,7 @@ class threatstack::configure {
   $ruleset_args = inline_template("<% @rulesets.each do |ruleset| -%> --ruleset='<%= ruleset %>'<% end -%>")
 
   exec { 'configure-threatstack-agent':
-    command     => "/opt/threatstack/bin/cloudsight setup --deploy-key='${::threatstack::deploy_key}' --hostname='${::threatstack::ts_hostname}' ${ruleset_args}",
+    command     => "/opt/threatstack/bin/cloudsight setup --deploy-key='${::threatstack::deploy_key}' --hostname='${::threatstack::ts_hostname}' ${ruleset_args} ${::threatstack::agent_extra_args}",
     subscribe   => Package[$threatstack::ts_package],
     creates     => '/opt/threatstack/cloudsight/config/.audit',
     path        => '/usr/bin'
