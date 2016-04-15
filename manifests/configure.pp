e7c4706a4785b6778b8f50693b8628e138a653b7
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-04-15 11:44:27 -0400

    use .audit over .secret which is a better case.

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -24,7 +24,7 @@ class threatstack::configure {
   exec { 'configure-threatstack-agent':
     command     => "/opt/threatstack/bin/cloudsight setup --deploy-key='${::threatstack::deploy_key}' --hostname='${::threatstack::ts_hostname}' ${ruleset_args}",
     subscribe   => Package[$threatstack::ts_package],
-    creates     => '/opt/threatstack/cloudsight/config/.secret',
+    creates     => '/opt/threatstack/cloudsight/config/.audit',
     path        => '/usr/bin'
   }
 
