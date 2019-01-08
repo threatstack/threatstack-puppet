f7e1b47972520bd24270fbb6e857eb74ce1a8e0a
nate st. germain <nate.stgermain@threatstack.com>
2019-01-07 22:26:08 -0500

    maybe cleaned this up a little

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
-describe 'ensure package resource' do
-  it 'creates a curl package resource in the catalogue' do
-    is_expected.to run.with_params('curl', {'ensure' => 'present'})
-    expect(catalogue).to contain_package('curl').with_ensure('present')
-  end
-end
-
 describe 'threatstack::apt' do
 
   deploy_key = ENV['TS_DEPLOY_KEY'] ? ENV['TS_DEPLOY_KEY'] : "xKkRzesqg"
@@ -16,15 +9,34 @@ describe 'threatstack::apt' do
     let(:facts) { {'os' => { 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} } }
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
-    it { should contain_exec('ts-agent-apt-get-update').with(
-      :command => 'apt-get update'
-    )}
-    it { should contain_exec('ts-gpg-import').with(
-      :command => 'curl https://app.threatstack.com/APT-GPG-KEY-THREATSTACK | apt-key add -'
+    #add real debian tests here
+  end
+
+  context 'on Ubuntu 18.04' do
+    let(:facts) { {'os' => { 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
+
+    it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
+      :owner => 'root',
+      :group => 'root',
+      :mode  => '0644',
+      :content => 'deb https://pkg.threatstack.com/Ubuntu bionic main'
     )}
   end
 
-  context 'on Ubuntu Trusy 14.04' do
+  context 'on Ubuntu 16.04' do
+    let(:facts) { {'os' => { 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
+
+    it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
+      :owner => 'root',
+      :group => 'root',
+      :mode  => '0644',
+      :content => 'deb https://pkg.threatstack.com/Ubuntu xenial main'
+    )}
+  end
+
+  context 'on Ubuntu 14.04' do
     let(:facts) { {'os' => { 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
