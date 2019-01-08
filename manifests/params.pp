64561d7cf7369fcd565ef76c148280d6e033bb94
nate st. germain <nate.stgermain@threatstack.com>
2019-01-08 11:57:14 -0500

    no top scope there?
    special var in play

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,14 +22,14 @@ class threatstack::params {
   $ts_service = 'cloudsight'
   $ruleset    = ['Base Rule Set']
 
-  case $::facts['os']['family'] {
+  case $facts['os']['family'] {
     'RedHat': {
       $repo_class       = '::threatstack::yum'
       $gpg_key          = 'https://app.threatstack.com/RPM-GPG-KEY-THREATSTACK'
       $gpg_key_file     = '/etc/pki/rpm-gpg/RPM-GPG-KEY-THREATSTACK'
       $gpg_key_file_uri = "file://${gpg_key_file}"
 
-      case $::facts['os']['name'] {
+      case $facts['os']['name'] {
         'Amazon': { $repo_url = 'https://pkg.threatstack.com/Amazon'}
         /(CentOS|RedHat)/: { $repo_url = "https://pkg.threatstack.com/EL/${::operatingsystemmajrelease}" }
         default: { fail("Module ${module_name} does not support ${::operatingsystem}") }
@@ -40,7 +40,7 @@ class threatstack::params {
       $repo_comment = 'Threat Stack agent repo'
       $repo_url     = 'https://pkg.threatstack.com/Ubuntu'
       $repo_gpg_id  = 'ACCC2B02EA3A2409557B0AB991BB3B3C6EE04BD4'
-      $release      = $::facts['os']['distro']['codename']
+      $release      = $facts['os']['distro']['codename']
       $repos        = 'main'
       $gpg_key      = 'https://app.threatstack.com/APT-GPG-KEY-THREATSTACK'
     }
