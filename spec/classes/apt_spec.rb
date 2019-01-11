da557695b657fb7409cda16bb7c1d1f1ac5ae692
nate st. germain <nate.stgermain@threatstack.com>
2019-01-11 15:37:04 -0500

    move defined type tests to apt class test

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -1,40 +1,79 @@
 require 'spec_helper'
 
 describe 'threatstack::apt' do
-
-  deploy_key = ENV['TS_DEPLOY_KEY'] ? ENV['TS_DEPLOY_KEY'] : "xKkRzesqg"
-  feature_plan = ENV['TS_FEATURE_PLAN'] ? ENV['TS_FEATURE_PLAN'] : "monitor"
+  GPG_KEY_ID = 'ACCC2B02EA3A2409557B0AB991BB3B3C6EE04BD4'.freeze
+  REPO_URL = 'https://pkg.threatstack.com/v2/Ubuntu'
+  REPO_TYPE = 'main'
 
   context 'on Debian 8' do
     let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
-    #add real debian tests here
+    let(:params) { { 'location' => REPO_URL, 'release' => 'jessie', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
+    it {
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb REPO_URL jessie main\n})
+                                                             .without_content(%r{deb-src})
+      }
+
+    it {
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
+      }
   end
 
   context 'on Debian 9' do
     let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '9.1', 'major' => '9', 'minor' => '1'}, 'distro' => {'codename' => 'stretch'}, 'family' => 'Debian'} } }
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'jessie', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
+    it {
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb REPO_URL stretch main\n})
+                                                             .without_content(%r{deb-src})
+      }
 
-    #add real debian tests here
+    it {
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
+      }
   end
 
   context 'on Ubuntu 18.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18.04', 'major' => '18.04'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'bionic', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
+    it {
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb REPO_URL bionic main\n})
+                                                             .without_content(%r{deb-src})
+      }
 
+    it {
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
+      }
   end
 
   context 'on Ubuntu 16.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '16.04', 'major' => '16.04'}, 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'xenial', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
+    it {
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb REPO_URL xenial main\n})
+                                                             .without_content(%r{deb-src})
+      }
 
+    it {
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
+      }
   end
 
   context 'on Ubuntu 14.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '14.04', 'major' => '14.04'}, 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'trust', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
+    it {
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb REPO_URL trusty main\n})
+                                                             .without_content(%r{deb-src})
+      }
 
+    it {
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
+      }
   end
 
 end
