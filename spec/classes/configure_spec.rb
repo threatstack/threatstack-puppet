bdbf0d6a31b212d95397560c47c8bec2d0171cae
nate st. germain <nate.stgermain@threatstack.com>
2019-01-11 13:29:16 -0500

    fix config spec
    needs more facts

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -6,8 +6,8 @@ describe 'threatstack::configure' do
   feature_plan = ENV['TS_FEATURE_PLAN'] ? ENV['TS_FEATURE_PLAN'] : "monitor"
   ts_hostname = 'test-host'
 
-  context 'on Debian' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
+  context 'on Debian 8' do
+    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}', ts_hostname => '#{ts_hostname}', ruleset => ['Default Ruleset', 'Service Ruleset'] }" }
 
     it { should contain_exec('threatstack-agent-setup').with(
