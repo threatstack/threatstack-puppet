534a714103b69631e26604250ff74068c6d04aa6
nate st. germain <nate.stgermain@threatstack.com>
2019-01-11 14:11:38 -0500

    facts for config spec

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -7,7 +7,7 @@ describe 'threatstack::configure' do
   ts_hostname = 'test-host'
 
   context 'on Debian 8' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
+    let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}', ts_hostname => '#{ts_hostname}', ruleset => ['Default Ruleset', 'Service Ruleset'] }" }
 
     it { should contain_exec('threatstack-agent-setup').with(
