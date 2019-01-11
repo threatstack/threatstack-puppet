ab0f4cfe416db1c4dcdf44669fd8fcf22abdc100
nate st. germain <nate.stgermain@threatstack.com>
2019-01-11 13:20:12 -0500

    fix facts in apt spec

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -6,21 +6,21 @@ describe 'threatstack::apt' do
   feature_plan = ENV['TS_FEATURE_PLAN'] ? ENV['TS_FEATURE_PLAN'] : "monitor"
 
   context 'on Debian 8' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
+    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     #add real debian tests here
   end
 
   context 'on Debian 9' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'distro' => {'codename' => 'stretch'}, 'family' => 'Debian'} } }
+    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', {'full' => '9.1', 'major' => '9', 'minor' => '1'}, 'distro' => {'codename' => 'stretch'}, 'family' => 'Debian'} } }
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     #add real debian tests here
   end
 
   context 'on Ubuntu 18.04' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
+      let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18.04', 'major' => '18.04'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
@@ -34,7 +34,7 @@ deb https://pkg.threatstack.com/v2/Ubuntu bionic main"
   end
 
   context 'on Ubuntu 16.04' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18'}, 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
+    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '16.04', 'major' => '16.04'}, 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
@@ -48,7 +48,7 @@ deb https://pkg.threatstack.com/v2/Ubuntu xenial main"
   end
 
   context 'on Ubuntu 14.04' do
-    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18'}, 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
+    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '14.04', 'major' => '14.04'}, 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
