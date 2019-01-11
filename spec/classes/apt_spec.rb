b82b85ec921ab8f29be856e71f26557e3d72f1ba
nate st. germain <nate.stgermain@threatstack.com>
2019-01-11 15:18:40 -0500

    move apt spec to defined type test

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -20,45 +20,21 @@ describe 'threatstack::apt' do
   end
 
   context 'on Ubuntu 18.04' do
-      let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18.04', 'major' => '18.04'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
+    let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18.04', 'major' => '18.04'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
-    it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
-      :owner => 'root',
-      :group => 'root',
-      :mode  => '0644',
-      :content => "# This file is managed by Puppet. DO NOT EDIT.
-# Threat Stack agent repo
-deb https://pkg.threatstack.com/v2/Ubuntu bionic main"
-    )}
   end
 
   context 'on Ubuntu 16.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '16.04', 'major' => '16.04'}, 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
-    it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
-      :owner => 'root',
-      :group => 'root',
-      :mode  => '0644',
-      :content => "# This file is managed by Puppet. DO NOT EDIT.
-# Threat Stack agent repo
-deb https://pkg.threatstack.com/v2/Ubuntu xenial main"
-    )}
   end
 
   context 'on Ubuntu 14.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '14.04', 'major' => '14.04'}, 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
-    it { should contain_file('/etc/apt/sources.list.d/threatstack.list').with(
-      :owner => 'root',
-      :group => 'root',
-      :mode  => '0644',
-      :content => "# This file is managed by Puppet. DO NOT EDIT.
-# Threat Stack agent repo
-deb https://pkg.threatstack.com/v2/Ubuntu trusty main"
-    )}
   end
 
 end
