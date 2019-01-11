ee057aafae3886f5eb950eecae8dbd5f9c8b91a8
nate st. germain <nate.stgermain@threatstack.com>
2019-01-11 15:59:01 -0500

    make it work

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -8,71 +8,71 @@ describe 'threatstack::apt' do
   context 'on Debian 8' do
     let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
 
-    let(:params) { { 'location' => $REPO_URL, 'release' => 'jessie', 'repos' => $REPO_TYPE, 'key' => $GPG_KEY_ID} }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'jessie', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb ${$REPO_URL} jessie main\n})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb  https://pkg.threatstack.com/v2/Ubuntu jessie main\n})
                                                              .without_content(%r{deb-src})
       }
 
     it {
-        is_expected.to contain_apt__key("Add key: #{$GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
-        id: $GPG_KEY_ID)
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
       }
   end
 
   context 'on Debian 9' do
     let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '9.1', 'major' => '9', 'minor' => '1'}, 'distro' => {'codename' => 'stretch'}, 'family' => 'Debian'} } }
-    let(:params) { { 'location' => $REPO_URL, 'release' => 'jessie', 'repos' => $REPO_TYPE, 'key' => $GPG_KEY_ID} }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'jessie', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb $REPO_URL stretch main\n})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu stretch main\n})
                                                              .without_content(%r{deb-src})
       }
 
     it {
-        is_expected.to contain_apt__key("Add key: #{$GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
-        id: $GPG_KEY_ID)
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
       }
   end
 
   context 'on Ubuntu 18.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18.04', 'major' => '18.04'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
-    let(:params) { { 'location' => $REPO_URL, 'release' => 'bionic', 'repos' => $REPO_TYPE, 'key' => $GPG_KEY_ID} }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'bionic', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb $REPO_URL bionic main\n})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu bionic main\n})
                                                              .without_content(%r{deb-src})
       }
 
     it {
-        is_expected.to contain_apt__key("Add key: #{$GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
-        id: $GPG_KEY_ID)
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
       }
   end
 
   context 'on Ubuntu 16.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '16.04', 'major' => '16.04'}, 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
-    let(:params) { { 'location' => $REPO_URL, 'release' => 'xenial', 'repos' => $REPO_TYPE, 'key' => $GPG_KEY_ID} }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'xenial', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb $REPO_URL xenial main\n})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu xenial main\n})
                                                              .without_content(%r{deb-src})
       }
 
     it {
-        is_expected.to contain_apt__key("Add key: #{$GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
-        id: $GPG_KEY_ID)
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
       }
   end
 
   context 'on Ubuntu 14.04' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '14.04', 'major' => '14.04'}, 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
-    let(:params) { { 'location' => $REPO_URL, 'release' => 'trust', 'repos' => $REPO_TYPE, 'key' => $GPG_KEY_ID} }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'trust', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb $REPO_URL trusty main\n})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu trusty main\n})
                                                              .without_content(%r{deb-src})
       }
 
     it {
-        is_expected.to contain_apt__key("Add key: #{$GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
-        id: $GPG_KEY_ID)
+        is_expected.to contain_apt__key("Add key: #{GPG_KEY_ID} from Apt::Source threatstack").that_comes_before('Apt::Setting[list-threatstack]').with(ensure: 'present',
+        id: GPG_KEY_ID)
       }
   end
 
--- spec/classes/package_spec.rb
+++ spec/classes/package_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 
 describe 'threatstack::package' do
 
-  deploy_key = ENV['TS_DEPLOY_KEY'] ? ENV['TS_DEPLOY_KEY'] : "xKkRzesqg"
-  feature_plan = ENV['TS_FEATURE_PLAN'] ? ENV['TS_FEATURE_PLAN'] : "monitor"
-
   context 'on RedHat' do
     let(:facts) {  { 'operatingsystem' => 'RedHat', 'operatingsystemmajrelease' => '7', 'os' => { 'release' => { 'full' => '7.5', 'major' => '7', 'minor' => '5'}, 'name' => 'RedHat', 'family' => 'RedHat'} } }
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     context 'package' do
       it { should contain_package('threatstack-agent').with_ensure('installed') }
@@ -16,7 +12,6 @@ describe 'threatstack::package' do
 
   context 'on Debian 8' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
-    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', feature_plan => '#{feature_plan}' }" }
 
     context 'package' do
       it { should contain_package('threatstack-agent').with_ensure('installed') }
