c6d5ced926f8a7044a793567075b46bd9ebe34a9
nate st. germain <nate.stgermain@threatstack.com>
2019-01-17 00:36:07 -0500

    dupe it up

--- spec/classes/configure_spec.rb
+++ spec/classes/configure_spec.rb
@@ -7,33 +7,67 @@ describe 'threatstack::configure' do
   ts_hostname = 'test-host'
 
   context 'on Debian 8' do
-    let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '8.11', 'major' => '8', 'minor' => '11'}, 'distro' => {'codename' => 'jessie'}, 'family' => 'Debian'} } }
     let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
 
     it { should contain_exec('threatstack-agent-setup').with(
-      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset 'Default Ruleset' --ruleset 'Service Ruleset'"
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
     )}
   end
 
   context 'on Debian 9' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
-  
+
   context 'on Ubuntu 18.04' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
 
   context 'on Ubuntu 16.04' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
 
   context 'on Ubuntu 14.04' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
 
   context 'on Redhat' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
 
   context 'on CentOS 7' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
 
   context 'on Amazon Linux' do
+    let(:pre_condition) { "class { 'threatstack': deploy_key => '#{deploy_key}', ts_hostname => '#{ts_hostname}', rulesets => ['Default Ruleset', 'Service Ruleset'], agent_config_args => [{'log.level' => 'debug'}] }" }
+
+    it { should contain_exec('threatstack-agent-setup').with(
+      :command => "/usr/bin/tsagent setup --deploy-key='#{deploy_key}' --hostname='#{ts_hostname}' --ruleset Default Ruleset --ruleset Service Ruleset"
+    )}
   end
 
 end
