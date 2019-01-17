230e7aa14c8a6b9a7a71cb6bf15ec34d89f74aea
nate st. germain <nate.stgermain@threatstack.com>
2019-01-16 23:39:49 -0500

    this might work for the apt spec test

--- spec/classes/apt_spec.rb
+++ spec/classes/apt_spec.rb
@@ -10,8 +10,7 @@ describe 'threatstack::apt' do
 
     let(:params) { { 'location' => REPO_URL, 'release' => 'jessie', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb  https://pkg.threatstack.com/v2/Ubuntu jessie main\n})
-                                                             .without_content(%r{deb-src})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present', location: REPO_URL, release: release, repos: REPO_TYPE)
       }
 
     it {
@@ -22,10 +21,9 @@ describe 'threatstack::apt' do
 
   context 'on Debian 9' do
     let(:facts) { {'operatingsystem' => 'Debian', 'osfamily' => 'Debian', 'os' => { 'name' => 'Debian', 'release' => {'full' => '9.1', 'major' => '9', 'minor' => '1'}, 'distro' => {'codename' => 'stretch'}, 'family' => 'Debian'} } }
-    let(:params) { { 'location' => REPO_URL, 'release' => 'jessie', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
+    let(:params) { { 'location' => REPO_URL, 'release' => 'stretch', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu stretch main\n})
-                                                             .without_content(%r{deb-src})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present', location: REPO_URL, release: release, repos: REPO_TYPE)
       }
 
     it {
@@ -38,8 +36,7 @@ describe 'threatstack::apt' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '18.04', 'major' => '18.04'}, 'distro' => {'codename' => 'bionic'}, 'family' => 'Debian'} }}
     let(:params) { { 'location' => REPO_URL, 'release' => 'bionic', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu bionic main\n})
-                                                             .without_content(%r{deb-src})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present', location: REPO_URL, release: release, repos: REPO_TYPE)
       }
 
     it {
@@ -52,8 +49,7 @@ describe 'threatstack::apt' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '16.04', 'major' => '16.04'}, 'distro' => {'codename' => 'xenial'}, 'family' => 'Debian'} }}
     let(:params) { { 'location' => REPO_URL, 'release' => 'xenial', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu xenial main\n})
-                                                             .without_content(%r{deb-src})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present', location: REPO_URL, release: release, repos: REPO_TYPE)
       }
 
     it {
@@ -66,8 +62,7 @@ describe 'threatstack::apt' do
     let(:facts) { {'osfamily' => 'Debian', 'os' => { 'name' => 'Ubuntu', 'release' => {'full' => '14.04', 'major' => '14.04'}, 'distro' => {'codename' => 'trusty'}, 'family' => 'Debian'} }}
     let(:params) { { 'location' => REPO_URL, 'release' => 'trust', 'repos' => REPO_TYPE, 'key' => GPG_KEY_ID} }
     it {
-        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present').with_content(%r{# threatstack\ndeb https://pkg.threatstack.com/v2/Ubuntu trusty main\n})
-                                                             .without_content(%r{deb-src})
+        is_expected.to contain_apt__setting('list-threatstack').with(ensure: 'present', location: REPO_URL, release: release, repos: REPO_TYPE)
       }
 
     it {
