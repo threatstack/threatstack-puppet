b2aac9fee9a37088ebafd958a9ec0bd14d63769b
nate st. germain <nate.stgermain@threatstack.com>
2019-10-16 20:29:43 -0400

    skip configure hile testing
    but get back to it

--- manifests/configure.pp
+++ manifests/configure.pp
@@ -19,54 +19,61 @@
 #
 class threatstack::configure {
 
-  $rulesets       = $::threatstack::rulesets
-  $ruleset_args   = $rulesets.map | $rule | {
-        "--ruleset='${rule}'"
-      }
-  if $::threatstack::extra_args {
-    $extra_args = $::threatstack::extra_args.map | $arg | {
-          "--${arg.keys[0]}=${arg.values[0]}"
+  case $facts['os']['family'] {
+    'Windows': {
+      notify("We are working on support for configuring the agent on ${::operatingsystem}")
+    }
+    default: {
+      $rulesets       = $::threatstack::rulesets
+      $ruleset_args   = $rulesets.map | $rule | {
+            "--ruleset='${rule}'"
+          }
+      if $::threatstack::extra_args {
+        $extra_args = $::threatstack::extra_args.map | $arg | {
+              "--${arg.keys[0]}=${arg.values[0]}"
+            }
+        $full_setup_args = "${join($ruleset_args, ' ')} ${join($extra_args, ' ')}"
+        } else {
+          $full_setup_args = "${join($ruleset_args, ' ')}"
         }
-    $full_setup_args = "${join($ruleset_args, ' ')} ${join($extra_args, ' ')}"
-    } else {
-      $full_setup_args = "${join($ruleset_args, ' ')}"
+
+      $cloudsight_bin = $::threatstack::cloudsight_bin
+      $confdir        = $::threatstack::confdir
+
+      if $::threatstack::agent_config_args {
+      $full_config_args = $::threatstack::agent_config_args.map | $config | {
+          "${config.keys[0]} ${config.values[0]}"
+        }
+      $config_args_content = join($full_config_args, "\n")
+
+      # this file tracks state and is used to notify
+      # Exec[threatstack-agent-configure] of the need to run.
+      file { "${confdir}/.config_args":
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => $config_args_content
+      }
+
+      exec { 'threatstack-agent-configure':
+        command     => "${cloudsight_bin} config -set ${join($full_config_args, ' ')}",
+        subscribe   => File["${confdir}/.config_args"],
+        refreshonly => true,
+        path        => ['/bin', '/usr/bin'],
+        notify      => Class['threatstack::service']
+      }
+      } else {
+          $config_args_content = ''
+      }
+
+      exec { 'threatstack-agent-setup':
+        command   => "${cloudsight_bin} setup --deploy-key='${::threatstack::deploy_key}' --hostname='${::threatstack::ts_hostname}' ${full_setup_args}",
+        subscribe => Package[$threatstack::ts_package],
+        creates   => "${confdir}/.audit",
+        path      => ['/bin', '/usr/bin'],
+        unless    => 'ps auwwwx| grep [t]sagentd'
+      }
     }
-
-  $cloudsight_bin = $::threatstack::cloudsight_bin
-  $confdir        = $::threatstack::confdir
-
-  if $::threatstack::agent_config_args {
-  $full_config_args = $::threatstack::agent_config_args.map | $config | {
-      "${config.keys[0]} ${config.values[0]}"
-    }
-  $config_args_content = join($full_config_args, "\n")
-
-  # this file tracks state and is used to notify
-  # Exec[threatstack-agent-configure] of the need to run.
-  file { "${confdir}/.config_args":
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => $config_args_content
-  }
-
-  exec { 'threatstack-agent-configure':
-    command     => "${cloudsight_bin} config -set ${join($full_config_args, ' ')}",
-    subscribe   => File["${confdir}/.config_args"],
-    refreshonly => true,
-    path        => ['/bin', '/usr/bin'],
-    notify      => Class['threatstack::service']
-  }
-  } else {
-      $config_args_content = ''
-  }
-
-  exec { 'threatstack-agent-setup':
-    command   => "${cloudsight_bin} setup --deploy-key='${::threatstack::deploy_key}' --hostname='${::threatstack::ts_hostname}' ${full_setup_args}",
-    subscribe => Package[$threatstack::ts_package],
-    creates   => "${confdir}/.audit",
-    path      => ['/bin', '/usr/bin'],
-    unless    => 'ps auwwwx| grep [t]sagentd'
   }
 }
