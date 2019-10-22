98e296301da1480731acd7934d0539f3de2bfb3e
nate st. germain <nate.stgermain@threatstack.com>
2019-10-22 15:12:53 -0400

    use full path here, too

--- manifests/sysmon.pp
+++ manifests/sysmon.pp
@@ -36,7 +36,7 @@ class threatstack::sysmon {
     command     => "C:\\Windows\\Temp\\Sysmon64.exe –accepteula –i sysmonconfig-export.xml",
     subscribe   => File['C:\Windows\Temp\sysmonconfig-export.xml'],
     refreshonly => true,
-    unless      => 'tasklist.exe /fi "Imagename eq sysmon*"',
+    unless      => 'C:\Windows\System32\tasklist.exe /fi "Imagename eq sysmon*"',
     require     => [
       Archive["C:\\Windows\\Temp\\sysmon.zip"],
       File['C:\Windows\Temp\sysmonconfig-export.xml']
